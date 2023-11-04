086d016089f12cc423c533c056b4ae05cace764e
Xian <qaq81704@gmail.com>
2023-11-05 01:30:07 +0800

    style💄: 更換設計圖片

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// Library
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-// Layouts
 import Main, { mainLoader } from "./layouts/Main";
-
-// Actions
 import { logoutAction } from "./actions/logout";
 import { deleteBudget } from "./actions/deleteBudget";
-
-// Routes
 import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 import BudgetPage, { budgetAction, budgetLoader } from "./pages/BudgetPage";
